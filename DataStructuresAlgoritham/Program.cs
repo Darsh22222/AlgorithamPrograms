75eb517e13d906c2e2c8bc1411ce884c1bec98a9
Darsh <darshankondhare2698@gmail.com>
2022-11-24 23:06:56 +0530

    [darsh] Add: Insertion Sort

--- DataStructuresAlgoritham/Program.cs
+++ DataStructuresAlgoritham/Program.cs
@@ -8,11 +8,33 @@ namespace DataStructuresAlgoritham
         static void Main(string[] args)
         {
             
-            Console.WriteLine("Welcome to DataStructureAlgoritham");
-            BinarySearch binarySearch = new BinarySearch();
-            binarySearch.ReadTextFile(FILE_PATH);
-            binarySearch.BinarySearchOperation("Hello, Welcome, To, Coding.");
 
+            Console.WriteLine("Welcome to DataStructureAlgoritham");
+            BinarySearch binary = new BinarySearch();
+            bool condition = true;
+            while (condition)
+            {
+                Console.WriteLine("1. Binary Search\n2. Insertion Sorting\n3. Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the word you want to Search");
+                        binary.ReadTextFile(FILE_PATH);
+                        binary.BinarySearchOperation("Hello,Welcome,To,Coding.");
+                        break;
+                    case 2:
+                        Console.WriteLine("\n");
+                        int[] arr = { 2, 5, 7, 6, 1, 4 };
+                        InsertionSort insertion = new InsertionSort();
+                        insertion.Sort(arr);
+                        break;
+                    default:
+                        Console.WriteLine("Try Again");
+                        condition = false;
+                        break;
+                }
+            }
 
         }
     }
