613c8691718bacc35bedc2ab65743f1647427170
Darsh <darshankondhare2698@gmail.com>
2022-11-24 22:34:39 +0530

    [darsh] Add: Binary Search Word from WordList

--- DataStructuresAlgoritham/Program.cs
+++ DataStructuresAlgoritham/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace DataStructuresAlgoritham
+﻿using System.Text;
+
+namespace DataStructuresAlgoritham
 {
     internal class Program
     {
+        const string FILE_PATH = @"C:\GetRepositry\AlgorithamPrograms\DataStructuresAlgoritham\BinarySearch.txt";
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Welcome to DataStructureAlgoritham");
+            BinarySearch binarySearch = new BinarySearch();
+            binarySearch.ReadTextFile(FILE_PATH);
+            binarySearch.BinarySearchOperation("Hello, Welcome, To, Coding.");
+
+
         }
     }
 }
